e74f15e5dfedd3e8ae3ab328ecaa5e597120b760
simjaeyoun <spqjfgogo@gmail.com>
2023-06-02 02:51:05 +0900

    test

---
+++ Client/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+public class TCPClient
+{
+    public static void Main()
+    {
+        string ipAdd="";
+        try
+        {
+            // Replace "example.com" with the hostname or domain name of the remote server
+            IPHostEntry hostEntry = Dns.GetHostEntry("example.com");
+
+            // Retrieve the IP addresses associated with the host
+            IPAddress[] addresses = hostEntry.AddressList;
+
+            // Print the IP addresses
+            foreach (IPAddress address in addresses)
+            {
+                ipAdd = address.ToString();
+                break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        // Set the server IP address and port
+        IPAddress serverIP = IPAddress.Parse(ipAdd); // Replace with the actual remote server IP address
+        int serverPort = 9998;
+
+        // Create a TCP client socket
+        TcpClient client = new TcpClient();
+
+        try
+        {
+            // Connect to the server
+            client.Connect(serverIP, serverPort);
+            Console.WriteLine("Connected to server: {0}:{1}", serverIP, serverPort);
+
+            // Get the client stream for reading and writing
+            NetworkStream stream = client.GetStream();
+
+            // Send data to the server
+            string message = "Hello from the client!";
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent to server: {0}", message);
+
+            // Read response from the server
+            byte[] responseBuffer = new byte[1024];
+            int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+            string response = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+            Console.WriteLine("Received from server: {0}", response);
+        }
+        finally
+        {
+            // Close the client socket
+            client.Close();
+            Console.WriteLine("Client disconnected.");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class TCPServer
+{
+    public static void Main()
+    {
+        // Set the listening IP address and port
+        IPAddress ipAddress = IPAddress.Any; // Listen on all available network interfaces
+        int port = 9998;
+
+        // Create a TCP listener
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+
+        Console.WriteLine("Server is listening on {0}:{1}", ipAddress, port);
+
+        try
+        {
+            while (true)
+            {
+                // Accept client connections
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected: {0}", client.Client.RemoteEndPoint);
+
+                // Handle client communication in a separate thread
+                System.Threading.Tasks.Task.Run(() =>
+                {
+                    // Get the client stream for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    // Read data from the client
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Received from client: {0}", dataReceived);
+
+                    // Process the received data (e.g., perform any necessary operations)
+
+                    // Send response to the client
+                    string response = "Hello from the server!";
+                    byte[] responseData = Encoding.ASCII.GetBytes(response);
+                    stream.Write(responseData, 0, responseData.Length);
+                    Console.WriteLine("Sent to client: {0}", response);
+
+                    // Close the client connection
+                    client.Close();
+                    Console.WriteLine("Client disconnected: {0}", client.Client.RemoteEndPoint);
+                });
+            }
+        }
+        finally
+        {
+            // Stop listening and clean up
+            listener.Stop();
+        }
+    }
+}
